020d0ce36a549b25d53641d5f36521db5c0e8ef4
lorenarios <lorenarosstqs1@gmail.com>
2024-11-12 22:57:59 -0500

    Vistas domiciliario (faltan cositas)

---
+++ Controllers/DomiciliarioController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRACKED.Controllers
+{
+    public class DomiciliarioController : Controller
+    {
+        // GET: Domiciliario
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Pedidos()
+        {
+            return View();
+        }
+        public ActionResult DetallesPedidos()
+        {
+            return View();
+        }
+        public ActionResult ProductosPedidos()
+        {
+            return View();
+        }
+    }
+}
